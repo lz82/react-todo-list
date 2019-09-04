31fefdfe9e3cbca0f20c7f080d54f1d75d758746
lz82 <nickylau82@gmail.com>
2019-09-04 22:19:53 +0800

    :construction: init react-redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,22 @@
 const defaultState = {
-  inputVal: '',
-  list: []
+  inputVal: 'hello',
+  list: ['todo1', 'todo2', 'todo3']
 }
 
 export default (state = defaultState, action) => {
-  return state
+  const newState = JSON.parse(JSON.stringify(state))
+  switch (action.type) {
+    case 'change_inputval':
+      newState.inputVal = action.value
+      return newState
+    case 'add_todo_item':
+      newState.list.push(newState.inputVal)
+      newState.inputVal = ''
+      return newState
+    case 'del_todo_item':
+      newState.list.splice(action.value, 1)
+      return newState
+    default:
+      return state
+  }
 }
--- src/todo-list/index.js
+++ src/todo-list/index.js
@@ -1,14 +1,76 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Input, Button, List } from 'antd'
 
 class TodoList extends Component {
   render () {
     return (
-      <div>
-        todo list
+      <div style={{width: 500, display: 'flex', flexFlow: 'column nowrap'}}>
+        <div style={{display: 'flex', flexFlow: 'row nowrap', paddingBottom: 20}}>
+          <Input 
+            style={{marginRight: 20}}
+            value={this.props.todoItem}
+            onChange={this.props.inputValChange}
+          />
+          <Button
+            type="primary"
+            onClick={this.props.addTodoItem}
+          >
+            增加
+          </Button>
+        </div>
+        <div>
+          <List
+            bordered
+            dataSource={this.props.list}
+            renderItem={(item, index) => {
+              return (
+                <List.Item
+                  onClick={() => this.props.delTodoItem(index)}
+                >
+                  {item}
+                </List.Item>
+              )
+            }}
+          >
+
+          </List>
+        </div>
       </div>
     )
   }
 }
 
-export default connect(null, null)(TodoList)
+const mapStateToProps = state => {
+  return {
+    todoItem: state.inputVal,
+    list: state.list
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    inputValChange: e => {
+      const action = {
+        type: 'change_inputval',
+        value: e.target.value
+      }
+      dispatch(action)
+    },
+    addTodoItem: () => {
+      const action = {
+        type: 'add_todo_item'
+      }
+      dispatch(action)
+    },
+    delTodoItem: index => {
+      const action = {
+        type: 'del_todo_item',
+        value: index
+      }
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
